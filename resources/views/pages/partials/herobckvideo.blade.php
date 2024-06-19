38c72553f35fd32fe4aa8d312fa0a5b0f86f7578
Francesco <francesco@cactus.polito.it>
2024-06-19 14:48:52 +0200

    WIP blocks

--- resources/views/pages/partials/herobckvideo.blade.php
+++ resources/views/pages/partials/herobckvideo.blade.php
@@ -1,20 +1,20 @@
 @php
     $videoUrl = is_array($block['data']['video']) ? reset($block['data']['video']) : $block['data']['video'];
 @endphp
-<section class="relative h-screen flex items-center justify-center overflow-hidden">
+<section class="relative flex items-center justify-center h-screen overflow-hidden">
     <video autoplay loop muted playsinline class="hero-bg-video" controls>
         <source src="{{ Storage::url($videoUrl) }}" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
 
     <div class="absolute inset-0 bg-gray-900 bg-opacity-50"></div>
-    <div class="relative text-center max-w-2xl mx-auto px-4">
-        <h1 class="text-white text-5xl font-bold">{{ $block['data']['title'] }}</h1>
-        <p class="text-white text-lg mt-4 mb-4">{{ $block['data']['subtitle'] }}</p>
+    <div class="relative max-w-2xl px-4 mx-auto text-center">
+        <h1 class="text-5xl font-bold text-white">{{ $block['data']['title'] }}</h1>
+        <p class="mt-4 mb-4 text-lg text-white">{{ $block['data']['subtitle'] }}</p>
         @foreach($block['data']['buttons'] as $button)
             <a href="{{ $button['button_url'] }}" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg {{ $loop->first ? 'bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900' : 'text-gray-300 hover:text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800' }}">
                 {{ $button['button_text'] }}
-                <x-icon name="{{ $button['icon'] }}" class="w-6 h-6 ml-2 flex-shrink-0"/>               
+                <x-icon name="{{ $button['icon'] }}" class="flex-shrink-0 w-6 h-6 ml-2"/>
             </a>
         @endforeach
     </div>
